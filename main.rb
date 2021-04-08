c1c5d434dad3c8ddf6e7265a0222a554f53cad02
Yukitaka-PG <alliswellthatendwell0@gmail.com>
2021-04-08 20:42:56 +0900

    勇者のセッター設定

--- main.rb
+++ main.rb
@@ -1,3 +1,23 @@
 class Brave
 
+   # nameのセッター
+   def name=(name)
+    @name = name
+  end
+
+  # hpのセッター
+  def hp=(hp)
+    @hp = hp
+  end
+
+  # offenseのセッター
+  def offense=(offense)
+    @offense = offense
+  end
+
+  # defenseのセッター
+  def defense=(defense)
+    @defense = defense
+  end
+
 end
