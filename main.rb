8e29928a4293ae1d5009d38bf7856ab75cc615d8
Yukitaka-PG <alliswellthatendwell0@gmail.com>
2021-04-09 18:42:19 +0900

    勇者の攻撃処理を実装

--- main.rb
+++ main.rb
@@ -10,20 +10,11 @@ class Brave
     @defense = params[:defense]
   end
 
+  def attack(monster)
+    puts "テスト：#{monster.name}のHPを表示 -> #{monster.hp}"
+  end
+
 end
-
-  brave = Brave.new(name:"テリー",hp:500,offense:150,defense:100)
-
-  puts <<~TEXT
-  NAME:#{brave.name}
-  HP:#{brave.hp}
-  OFFENSE:#{brave.offense}
-  DEFENSE:#{brave.defense}
-  TEXT
-
-  brave.hp -= 30
-
-  puts "#{brave.name}はダメージを受けた！残りのHPは#{brave.hp}だ"
 
 class Monster
   
@@ -39,11 +30,6 @@ class Monster
 
 end
 
-  monster = Monster.new(name:"スライム",hp:250,offense:100,defense:50)
-
-  puts <<~TEXT
-  NAME:#{monster.name}
-  HP:#{monster.hp}
-  OFFENSE:#{monster.offense}
-  DEFENSE:#{monster.defense}
-  TEXT
+  brave = Brave.new(name:"テリー",hp:500,offense:150,defense:100)
+  monster = Monster.new(name:"スライム",hp:250,offense:200,defense:100)
+  brave.attack(monster)
